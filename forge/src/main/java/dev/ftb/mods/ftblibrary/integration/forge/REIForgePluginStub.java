fa60b67386054031d7c2c2d9326c2fd036cd03c3
HUANGHU1 <907322015@qq.com>
2024-07-13 00:10:05 +0800

    fix: crash when runing on server

--- forge/src/main/java/dev/ftb/mods/ftblibrary/integration/forge/REIForgePluginStub.java
+++ forge/src/main/java/dev/ftb/mods/ftblibrary/integration/forge/REIForgePluginStub.java
@@ -1,10 +1,10 @@
 package dev.ftb.mods.ftblibrary.integration.forge;
 
 import dev.ftb.mods.ftblibrary.integration.REIIntegration;
-import me.shedaniel.rei.forge.REIPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
 import me.shedaniel.rei.forge.REIPluginCommon;
 import net.minecraftforge.api.distmarker.Dist;
 
-@REIPluginCommon
+@REIPluginClient
 public class REIForgePluginStub extends REIIntegration {
 }
